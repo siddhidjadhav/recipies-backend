73f1e24d031fb39eaaa52996a956476db318416e
siddhi jadhav <siddhidilipjadhav2000@gmail.com>
2024-07-17 20:16:46 -0700

    functionality for post request

--- recipies/urls.py
+++ recipies/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import RecipeList
+from .views import RecipeList, create_recipe
 
 urlpatterns = [
     path('recipies/', RecipeList.as_view()),
+    path('create/', create_recipe, name='create_recipe'),
 ]
--- recipies/views.py
+++ recipies/views.py
@@ -1,7 +1,19 @@
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Recipe
 from .serializers import RecipeSerializer
 
 class RecipeList(generics.ListCreateAPIView):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
+
+@api_view(['POST'])
+def create_recipe(request):
+    if request.method == 'POST':
+        serializer = RecipeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
