6e2c312b409ba255e64fdaf17e48edcee0670239
siddhi jadhav <siddhidilipjadhav2000@gmail.com>
2024-07-18 22:55:26 -0700

    updated urls

--- recipies/urls.py
+++ recipies/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import RecipeList, create_recipe
+from .views import RecipeList, create_recipe, DeleteAllRecipes, RecipeDetail
 
 urlpatterns = [
     path('recipies/', RecipeList.as_view()),
     path('create/', create_recipe, name='create_recipe'),
+    path('recipes/delete/', DeleteAllRecipes.as_view(), name='delete-all-recipes'),
+    path('recipes/<int:pk>/', RecipeDetail.as_view(), name='recipe-detail'),
 ]
--- recipies/views.py
+++ recipies/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -17,3 +18,12 @@ def create_recipe(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class DeleteAllRecipes(APIView):
+    def delete(self, request):
+        Recipe.objects.all().delete()
+        return Response({"message": "All recipes deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
+    
+class RecipeDetail(generics.RetrieveAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
